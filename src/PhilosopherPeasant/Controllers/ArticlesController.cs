0847796e72e339843631352334d0417e0d501ac0
David Diehr <ddiehr@yahoo.com>
2016-04-27 21:59:19 -0700

    Updates CRUD. But I still suck, so it doesn't work.

--- src/PhilosopherPeasant/Controllers/ArticlesController.cs
+++ src/PhilosopherPeasant/Controllers/ArticlesController.cs
@@ -3,17 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Microsoft.Data.Entity;
+using Microsoft.AspNet.Mvc.Rendering;
+using PhilosopherPeasant.Models;
 
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
 
 namespace PhilosopherPeasant.Controllers
 {
     public class ArticlesController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private ApplicationDbContext db;
+        public ArticlesController(ApplicationDbContext context)
         {
+            db = context;
+        }
+        public IActionResult Index(int id)
+        {
+          var articles = db.Articles.Where(x => x.WriterId == id).Include(x => x.Writer).ToList();
+
+          if(articles.Count >= 1)
+          {
+              return View(articles);
+          }
+          else
+          {
+              return View();
+          }
+        }
+        public IActionResult Create()
+        {
+            ViewBag.WriterId = new SelectList(db.Writers, "WriterId", "Name");
+
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Article article)
+        {
+            db.Articles.Add(article);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Writers");
+        }
+        public IActionResult Edit(int id)
+        {
+            var thisArticle = db.Articles.FirstOrDefault(x => x.ArticleId == id);
+
+            ViewBag.WriterId = new SelectList(db.Writers, "WriterId", "Name");
+
+            return View(thisArticle);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Article article)
+        {
+            db.Entry(article).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index", "Writers");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var thisArticle = db.Articles.FirstOrDefault(x => x.ArticleId == id);
+            db.Articles.Remove(thisArticle);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Writers");
         }
     }
 }
--- src/PhilosopherPeasant/Controllers/WritersController.cs
+++ src/PhilosopherPeasant/Controllers/WritersController.cs
@@ -3,17 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Microsoft.Data.Entity;
+using PhilosopherPeasant.Models;
+using Microsoft.AspNet.Mvc.Rendering;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace PhilosopherPeasant.Controllers
 {
-    public class AuthorsController : Controller
+    public class WritersController : Controller
     {
-        // GET: /<controller>/
+        private ApplicationDbContext db;
+        public WritersController(ApplicationDbContext context)
+        {
+            db = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(db.Writers.ToList());
+        }
+        public IActionResult Create()
+        {
+          return View();
+        }
+        [HttpPost]
+        public IActionResult Create (Writer writer)
+        {
+          db.Writers.Add(writer);
+          db.SaveChanges();
+          return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            var writer = db.Writers.FirstOrDefault(w => w.WriterId == id);
+            return View(writer);
+        }
+        [HttpPost]
+        public IActionResult Edit(Writer writer)
+        {
+            db.Entry(writer).State = Microsoft.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+          var thisWriter = db.Writers.FirstOrDefault(w => w.WriterId == id);
+          db.Writers.Remove(thisWriter);
+          db.SaveChanges();
+          return RedirectToAction("Index");
         }
     }
 }
