8b28f0c48ab3702f4d97d1e974a0f0a97b352bb1
David Diehr <ddiehr@yahoo.com>
2016-04-29 14:51:38 -0700

    Adds Admin controller and view.

---
+++ src/PhilosopherPeasant/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Authorization;
+
+
+namespace PhilosopherPeasant.Controllers
+{
+    [Authorize(Roles="Admin")]
+    public class AdminController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
