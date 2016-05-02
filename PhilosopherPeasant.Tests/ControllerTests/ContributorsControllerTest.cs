7d03c84d6fb97c4a72f83100b0157ba6f77f47e6
David Diehr <ddiehr@yahoo.com>
2016-05-01 20:02:45 -0700

    Add preliminary model and cohntroller tests.

---
+++ PhilosopherPeasant.Tests/ControllerTests/ContributorsControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Data.Entity;
+using PhilosopherPeasant.Controllers;
+using PhilosopherPeasant.Models;
+using Xunit;
+
+namespace PhilosopherPeasant.Tests
+{
+    public class ContributorsControllerTest
+    {
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            //Arrange
+            ContributorsController controller = new ContributorsController();
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
---
+++ PhilosopherPeasant.Tests/ModelTests/Contributor.cs
@@ -0,0 +1,21 @@
+﻿using PhilosopherPeasant.Models;
+using Xunit;
+
+namespace PhilosopherPeasant.Tests
+{
+    public class ContributorTest
+    {
+        [Fact]
+        public void GetNameTest()
+        {
+            //Arrange
+            var contributor = new Contributor();
+            contributor.Name = "David Diehr";
+            //Act
+            var result = contributor.Name;
+
+            //Assert
+            Assert.Equal("David Diehr", result);
+        }
+    }
+}
